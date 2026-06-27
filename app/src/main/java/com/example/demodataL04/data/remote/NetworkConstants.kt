493e5cb661cd7386b54bc531c93ed4ffaa39bbe4
AldairLeonH <aldairanthony.leon@unmsm.edu.pe>
2026-06-27 09:27:16 -0500

    labo 07 prueba

---
+++ app/src/main/java/com/example/demodataL04/data/remote/NetworkConstants.kt
@@ -0,0 +1,8 @@
+package com.example.demodataL04.data.remote
+
+object NetworkConstants {
+    const val BASE_URL     = "https://platform-api.kankunapaq.com/"
+    // const val BASE_URL  = "http://illarek.org/"
+
+    const val PROJECT_SLUG = "layout_example"
+}
